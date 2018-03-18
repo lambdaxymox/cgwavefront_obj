a6c0264ab57d02f71d5988d12490e7ea5d573ca5
LambdaXymox <lambda.xymox@gmail.com>
2018-03-18 06:29:10 +0000

    Define the ontology for a wavefront obj file's mesh when loaded.

--- src/obj/obj.rs
+++ src/obj/obj.rs
@@ -1,11 +1,3 @@
-
-pub struct Mesh {
-    name: String,
-    vertices: Vec<Vertex>,
-    texture_vertices: Vec<TextureVertex>,
-    normals: Vec<NormalVector>,
-}
-
 #[derive(Clone, Copy, Debug)]
 pub struct Vertex {
     pub x: f32,
@@ -15,7 +7,7 @@ pub struct Vertex {
 }
 
 #[derive(Clone, Copy, Debug)]
-pub struct NormalVector {
+pub struct NormalVertex {
     pub i: f32,
     pub j: f32,
     pub k: f32,
@@ -28,18 +20,48 @@ pub struct TextureVertex {
     pub w: f32,
 }
 
-type VIndex = usize;
+type GroupName = String;
+type SmoothingGroupName = String;
+
+type VertexIndex = u32;
+type TextureIndex = u32;
+type NormalIndex = u32;
+type GroupNameIndex = u32;
+type ShapeIndex = u32;
+type ElementIndex = u32;
+type SmoothingGroupIndex = u32;
+
+struct VertexSet(Vec<Vertex>);
+struct TextureVertexSet(Vec<TextureVertex>);
+struct NormalVertexSet(Vec<NormalVertex>);
+struct GroupSet(Vec<GroupName>);
+struct ElementSet(Vec<Element>);
+struct SmoothingGroupSet(Vec<SmoothingGroupName>);
+struct ShapeSet(Vec<Shape>);
 
 enum Element {
-    Point(VIndex),
-    Line(VIndex, VIndex),
-    Face(VIndex, VIndex, VIndex),
+    Point(VertexIndex),
+    Line(VertexIndex, VertexIndex),
+    Face(VertexIndex, VertexIndex, VertexIndex),
 }
-
-type GroupName = String;
 
 struct Shape {
     element: Element,
-    groups: Vec<GroupName>,
-    smoothing_groups: Vec<u32>,
+    groups: Vec<GroupNameIndex>,
+    smoothing_groups: Vec<SmoothingGroupIndex>,
+}
+
+pub struct Object {
+    name: String,
+    vertex_set: Vec<Vertex>,
+    texture_vertex_set: Vec<TextureVertex>,
+    normal_vertex_set: Vec<NormalVertex>,
+    group_set: Vec<GroupName>,
+    smoothing_group_set: Vec<SmoothingGroupName>,
+    element_set: Vec<Element>,
+    shape_set: Vec<Shape>,
+}
+
+pub struct ObjectSet {
+    objects: Vec<Object>,
 }
