74a2effe6868b5e14b7a4c04c543162f31f22966
LambdaXymox <lambda.xymox@gmail.com>
2018-03-23 18:01:27 +0000

    Add units tests back to vertex parser.

--- src/obj/vertex_parser.rs
+++ src/obj/vertex_parser.rs
@@ -27,11 +27,12 @@ impl VertexParser {
 #[cfg(test)]
 mod tests {
     use obj::object::Vertex;
-    
+    use obj::parser::ParserState;
+
 
     #[test]
     fn test_parse_vertex1() {
-        let mut parser_state = super::ParserState::new("v -1.929448 13.329624 -5.221914\n");
+        let mut parser_state = ParserState::new("v -1.929448 13.329624 -5.221914\n");
         let parser = super::VertexParser::new();
         let vertex = Vertex { x: -1.929448, y: 13.329624, z: -5.221914, w: 1.0 };
         assert_eq!(parser.parse(&mut parser_state), Ok(vertex));
@@ -39,7 +40,7 @@ mod tests {
 
     #[test]
     fn test_parse_vertex2() {
-        let mut parser_state = super::ParserState::new("v -1.929448 13.329624 -5.221914 1.329624\n");
+        let mut parser_state = ParserState::new("v -1.929448 13.329624 -5.221914 1.329624\n");
         let parser = super::VertexParser::new();
         let vertex = Vertex { x: -1.929448, y: 13.329624, z: -5.221914, w: 1.329624 };
         assert_eq!(parser.parse(&mut parser_state), Ok(vertex));
@@ -47,9 +48,34 @@ mod tests {
 
     #[test]
     fn test_parse_vertex3() {
-        let mut parser_state = super::ParserState::new("v -1.929448 13.329624 \n");
+        let mut parser_state = ParserState::new("v -1.929448 13.329624 \n");
         let parser = super::VertexParser::new();
         assert!(parser.parse(&mut parser_state).is_err());
+    }
+
+    #[test]
+    fn test_parse_vertex4() {
+        let mut parser_state = ParserState::new("v -1.929448 13.329624 -5.221914 1.329624\n v");
+        let parser = super::VertexParser::new();
+        assert!(parser.parse(&mut parser_state).is_ok());
+    }
+
+    #[test]
+    fn test_parse_vertex5() {
+        let mut parser_state = ParserState::new(
+             "v -6.207583 1.699077 8.466142
+              v -14.299248 1.700244 8.468981 1.329624"
+        );
+        let parser = super::VertexParser::new();
+        assert_eq!(
+            parser.parse(&mut parser_state), 
+            Ok(Vertex { x: -6.207583, y: 1.699077, z: 8.466142, w: 1.0 })
+        );
+        assert_eq!(parser_state.next(), Some(String::from("\n")));
+        assert_eq!(
+            parser.parse(&mut parser_state), 
+            Ok(Vertex { x: -14.299248, y: 1.700244, z: 8.468981, w: 1.329624 })
+        );
     }
 }
 
