d95dfaf6973591ea74d4893329faf0eb4bd46553
LambdaXymox <lambda.xymox@gmail.com>
2018-04-18 21:14:58 +0000

    Update example for new interface.

--- examples/teapot.rs
+++ examples/teapot.rs
@@ -1,23 +1,14 @@
 extern crate wavefront;
 
-
-use wavefront::obj::Parser;
-
+use wavefront::obj;
 use std::fs::File;
-use std::io::{BufReader, Read};
-
 
 const SAMPLE_DATA: &str = "sample_data/teapot.obj";
 
 
 fn main() {
     let file = File::open(SAMPLE_DATA).expect("File not found.");
-    let mut reader = BufReader::new(file);
-    let mut string = String::new();
-    reader.read_to_string(&mut string).unwrap();
-
-    let mut parser = Parser::new(string.chars());
-    let object_set = parser.parse().expect(&format!("Failed to parse {}.", SAMPLE_DATA));
+    let object_set = obj::parse(file).expect(&format!("Failed to parse {}.", SAMPLE_DATA));
     println!("{}", object_set);
 }
 
